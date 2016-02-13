ead3fd0eb0d5df77b1550baa4ea351b18bd3722d
math2k <m@th2k.net>
2016-02-13 21:57:05 +0100

    Added notification request to admin

--- app/admin.py
+++ app/admin.py
@@ -2,17 +2,21 @@ from django.contrib.admin import ModelAdmin
 from app.models import *
 from django.contrib import admin
 
+
 class UserOrderAdmin(ModelAdmin):
     list_display = ('name', 'order', 'total', 'paid')
     list_filter = ('name',)
+
 
 class MenuItemAdmin(ModelAdmin):
     list_display = ('name', 'unit_price', 'category', 'provider')
     list_filter = ('category', 'category__provider')
 
+
 class MenuItemCategoryAdmin(ModelAdmin):
     list_display = ('name', 'provider')
     list_filter = ('provider',)
+
 
 class OrderAdmin(ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
@@ -20,10 +24,11 @@ class OrderAdmin(ModelAdmin):
             r_parts = request.path.split('/')
             order_id = r_parts[-2]
             try:
-              kwargs["queryset"] = UserOrder.objects.filter(order=order_id)
+                kwargs["queryset"] = UserOrder.objects.filter(order=order_id)
             except ValueError:
-              pass
+                pass
         return super(OrderAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 admin.site.register(Order, OrderAdmin)
 admin.site.register(UserOrder, UserOrderAdmin)
@@ -31,3 +36,4 @@ admin.site.register(MenuItem, MenuItemAdmin)
 admin.site.register(MenuItemCategory, MenuItemCategoryAdmin)
 admin.site.register(UserOrderItem)
 admin.site.register(FoodProvider)
+admin.site.register(NotificationRequest)
